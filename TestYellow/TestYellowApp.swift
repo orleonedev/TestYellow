94403cedb27716ea1ff35a0e7663b4474920bd65
Oreste Leone <92297799+orleonedev@users.noreply.github.com>
2022-10-30 10:57:17 +0100

    Initial Commit

---
+++ TestYellow/TestYellowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestYellowApp.swift
+//  TestYellow
+//
+//  Created by Oreste Leone on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TestYellowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
